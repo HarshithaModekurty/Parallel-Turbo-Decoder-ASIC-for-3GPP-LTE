a553d10c71384e072b74ce19f30182e414d2970f
HarshithaModekurty <harshithamodekurty@gmail.com>
2026-03-23 22:23:31 +0530

    Add testbench configuration for tb_turbo_top
    - Created a new TCL script for the tb_turbo_top testbench.
    - Added wave signals for clock, reset, input/output validity, and other relevant signals.
    - Set the simulation to run for 400 microseconds.

---
+++ tb_turbo_top.tcl
@@ -0,0 +1,22 @@
+if {[string length [current_wave_config]] == 0} {
+  create_wave_config
+}
+
+add_wave /tb_turbo_top/clk
+add_wave /tb_turbo_top/rst
+add_wave /tb_turbo_top/start
+add_wave /tb_turbo_top/in_valid
+add_wave /tb_turbo_top/in_idx
+add_wave /tb_turbo_top/ls
+add_wave /tb_turbo_top/lp1
+add_wave /tb_turbo_top/lp2
+add_wave /tb_turbo_top/n_half_iter
+add_wave /tb_turbo_top/k_len
+add_wave /tb_turbo_top/f1
+add_wave /tb_turbo_top/f2
+add_wave /tb_turbo_top/out_valid
+add_wave /tb_turbo_top/out_idx
+add_wave /tb_turbo_top/post
+add_wave /tb_turbo_top/done
+
+run 400 us
